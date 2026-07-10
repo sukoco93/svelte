61efdfc619ad739db717de1d758b59a4fbb17047
sukoco93 <82564991+sukoco93@users.noreply.github.com>
2026-07-10 17:31:41 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  // ⚠️ Ganti 'svelte-spa-app' dengan NAMA REPO KAMU!
+  base: '/svelte/', 
   plugins: [svelte(), tailwindcss()],
 });
